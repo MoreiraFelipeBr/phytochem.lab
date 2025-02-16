0b7ebe34a86d7988268b123615bbd49ff6e7f972
Felipe Moreira <164668169+MoreiraFelipeBr@users.noreply.github.com>
2025-02-16 20:51:17 -0300

    123

--- script/script.js
+++ script/script.js
@@ -1,4 +1,28 @@
-let lastScroll = window.scrollY;
+const header = document.getElementById("header");
+const contatosTop = document.getElementById("contatosTop");
+const imgLogo = document.getElementById("imglogo");
+
+window.addEventListener("scroll", () => {
+    let scrollPosition = window.scrollY; // Obtém a posição atual do scroll
+
+    if (scrollPosition > 10) { // Se passou de 100px, esconde contatosTop
+        header.classList.add("header-fixed");
+        header.style.top = "0";
+        document.querySelector(".header-content").style.border = "none";
+        header.style.backgroundColor = "white";
+        contatosTop.classList.add("hidden"); 
+        document.getElementById("imglogo").src = "img/logo-blue.png";
+    } else { // Se está no topo, mantém tudo visível
+        document.getElementById("imglogo").src = "img/logo-white.png";
+        header.classList.remove("header-fixed");
+        header.style.top = "0";
+        document.querySelector(".header-content").style.border = "";
+        header.style.backgroundColor = "";
+        contatosTop.classList.remove("hidden"); 
+    }
+});
+
+/* let lastScroll = window.scrollY;
 const header = document.getElementById("header");
 const contatosTop = document.getElementById("contatosTop");
 
@@ -6,17 +30,25 @@ window.addEventListener("scroll", () => {
     let currentScroll = window.scrollY;
 
     if (currentScroll > lastScroll) {
-        headerMovel.classList.add("hidden");
-        contatosTop.classList.add("hidden"); // Esconde contatos-top
-        /* header.classList.add("reduzido"); // Reduz a altura do header */
+        
+        header.classList.add("header-fixed");
+        header.style.top = "0";
+        document.querySelector(".header-content").style.border = "none";
+        header.style.backgroundColor = "white";
+        contatosTop.classList.add("hidden"); 
+        document.getElementById("imglogo").src = "img/logo-blue.png";
     } else {
-        headerMovel.classList.remove("hidden");
-        contatosTop.classList.remove("hidden"); // Mostra contatos-top
-        /* header.classList.remove("reduzido"); // Restaura a altura original do header */
+        document.getElementById("imglogo").src = "img/logo-white.png";
+        header.classList.remove("header-fixed");
+        header.style.top = "0";
+        document.querySelector(".header-content").style.border = "";
+        header.style.backgroundColor = "";
+        contatosTop.classList.remove("hidden"); 
     }
 
     lastScroll = currentScroll;
-});
+}); 
+*/
 
 // Menu
 document.addEventListener('DOMContentLoaded', function () {
@@ -38,6 +70,6 @@ document.addEventListener('DOMContentLoaded', function () {
         menu.style.right = (menu.offsetWidth * -1) + 'px';
         setTimeout(() => {
             menu.style.display = 'none';
-        }, 300); // Tempo de transição correspondente ao CSS
+        }, 300); 
     });
 });
